d1ff724cbbcbd91c1e9b25e6aab4a37c59b0bba7
Clément Llobet <clement.llobet@tutanota.com>
2022-02-02 17:04:16 +0100

    create method in fetch to show product in index.html files

--- front/js/main.js
+++ front/js/main.js
@@ -1,7 +1,21 @@
+// Méthdde de requête à l'API pour récupérer l'ensemble des produits et insérer chaque élément dans la page d'accueil
 fetch("http://localhost:3000/api/products")
     .then(response => response.json())
     .then((data) => {
-        let array = data;
-        console.log(array);
+
+        let producList = data;
+        let items = document.getElementById("items");
+
+        items.innerHTML = producList.map(product => `
+            <a href="./product.html?id=${product._id}">
+                <article>
+                    <img src="${product.imageUrl}" alt="${product.altTxt}">
+                    <h3 class="productName">${product.name}</h3>
+                    <p class="productDescription">${product.description}</p>
+                </article>
+            </a>`
+        ) 
     })
     .catch(error => "L'erreur suivante est survenue : " + error)
+
+
