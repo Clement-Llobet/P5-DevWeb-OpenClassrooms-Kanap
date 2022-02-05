a74292d61550778cbf7a92b47200bd4ecab68bca
Clément Llobet <clement.llobet@tutanota.com>
2022-02-05 14:29:21 +0100

    correct import infos bug

--- front/js/product.js
+++ front/js/product.js
@@ -2,12 +2,16 @@ const getUrlId = () => {
     let url = new URL(window.location.href);
     let searchParams = new URLSearchParams(url.search);
     
-    if(searchParams.has("id")) {
+    if (searchParams.has("id")) {
         let id = searchParams.get("id");
+        return id
     } else {
-        console.log(`Erreur 404 : cette page n'existe pas.`);
+        alert(`Erreur 404 : cette page n'existe pas.`);
+        window;location.href = "../index.html"
     }
 }
+
+let thisID = getUrlId();
 
 let productImage = document.querySelector("div.item__img");
 let productTitle = document.getElementById("title");
@@ -15,25 +19,24 @@ let productPrice = document.getElementById("price");
 let productDescription = document.getElementById("description");
 let productSelectOptions = document.getElementById("colors");
 
-let thisId = getUrlId();
-
 fetch("http://localhost:3000/api/products")
     .then(response => response.json())
     .then((data) => {
         
         for (let i = 0; i < data.length; i++) {
-            if (data[i].id === thisId) {
+            if (data[i]._id === thisID) {
                 productImage.innerHTML = `<img src="${data[i].imageUrl}" alt="${data[i].altTxt}">`;
                 productTitle.innerHTML = `${data[i].name}`;
                 productPrice.innerHTML = `${data[i].price}`;
                 productDescription.innerHTML = `${data[i].description}`;
                 
                 productColors = data[i].colors;
-                productSelectOptions.innerHTML = productColors.map(color =>
-                    `<option value="${color}">${color}</option>`
-                    )
+                productColors.forEach(color => {
+                    let newOption = new Option(`${color}`, `${color}`);
+                    productSelectOptions.appendChild(newOption);
+                });
                 
-                    document.title = `${data[i].name}`;
+                document.title = `${data[i].name}`;
                 
                 break;
             }
